3c0c41948ad66b5d55f4295d7d5b000478e9644e
Egoraizer <eqop2208@gmail.com>
2023-02-14 17:12:56 +0300

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
-  // import Swiper JS
-  import Swiper from 'swiper';
-  // import Swiper styles
+  import Swiper, { Navigation, Pagination, Grid } from 'swiper';
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const sw1 = new Swiper('.swiper', {
+      modules: [Pagination, Grid],
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        renderBullet: function(index, className) {
+          return (
+            '<div class="' + className + '">' + '</div>'
+          )
+        }
+      },
+
+      grid: {
+        rows: 2,
+      },
+    })
+  })
